96e4591163cc24896513c861eb161664c5dea3a9
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-21 23:50:50 -0400

    test and code for popover appearing on hover and disappearing on mouse out

--- src/pages/summary/SummaryFormMui.jsx
+++ src/pages/summary/SummaryFormMui.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 // import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-// import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-
-// const useStyles = makeStyles((theme) => ({
-//   terms: {
-//     color: "blue",
-//   },
-// }));
 
 const SummaryForm = () => {
   // const classes = useStyles();
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,10 @@
 import SummaryForm from "../SummaryForm";
-import { render, screen } from "@testing-library/react";
-import userEvent  from "@testing-library/user-event";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 // TODO: test to check that checkbox is initially unchecked
 // TODO: test to check that button is initially disabled
@@ -10,9 +14,9 @@ import userEvent  from "@testing-library/user-event";
 test("checking checkbox enables button and unchecking it disables button", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: "I agree to Terms and Conditions",
+    name: /Terms and Conditions/i,
   });
-  const confirmButton = screen.getByRole("button", { name: "Confirm Order" });
+  const confirmButton = screen.getByRole("button", { name: /Confirm Order/i });
 
   // initial conditions of checkbox and button
   expect(checkbox).not.toBeChecked();
@@ -27,4 +31,27 @@ test("checking checkbox enables button and unchecking it disables button", () =>
   userEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseOver over checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears on mouseOut of checkbox label
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
