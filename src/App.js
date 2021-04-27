e2d51ac287f4122d1bd91e21341a659c99565920
Desmond Tuiyot <dtuiyot@gmail.com>
2021-04-27 13:20:03 -0400

    test and code for alert from server error response

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import SummaryForm from "./pages/summary/SummaryForm";
+// import SummaryForm from "./pages/summary/SummaryForm";
 import Options from "./pages/entry/Options";
 
 function App() {
